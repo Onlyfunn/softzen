0653858f131a415bfd78cec74d34e6c91517ed67
Onlyfunn <mitrofanovan@mail.ru>
2025-05-19 12:16:04 +0500

    First Commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+new WOW().init();
+
+const mainSlider = new Swiper(".main-info__swiper", {
+  loop: true,
+  direction: "horizontal",
+  speed: 500,
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: ".main-info__swiper-pagination",
+    clickable: true,
+  },
+});
+
+const examplesSlider = new Swiper(".examples__swiper", {
+  loop: false,
+  direction: "horizontal",
+  slidesPerView: 3,
+  spaceBetween: 21,
+  speed: 500,
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: true,
+  },
+
+  pagination: {
+    el: ".examples__swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    prevEl: ".examples__swiper-button-prev",
+    nextEl: ".examples__swiper-button-next",
+  },
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.code == "KeyT") {
+    if (document.body.classList.contains("light-theme")) {
+      document.body.classList.remove("light-theme");
+      document.body.classList.add("dark-theme");
+    } else {
+      document.body.classList.add("light-theme");
+      document.body.classList.remove("dark-theme");
+    }
+  }
+});
